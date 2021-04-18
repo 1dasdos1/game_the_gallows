c2de7108a7c7c7ba34d761a0bfd6fab79ddddfd7
Marat Ahmetzyanov <77569540+1dasdos1@users.noreply.github.com>
2021-04-18 14:35:17 +0400

    Add files via upload

--- game.py
+++ game.py
@@ -11,21 +11,25 @@ RED = (225, 225, 225)
 GREEN = (225, 225, 225)
 BLUE = (225, 225, 255)
 
-WRONG = 0
-RIGHT = 1
+NULL = 0
+WRONG = 1
+RIGHT = 2
 
 LINE_WIDTH = 4
+CIRCLE_WIDTH = 4
 
-keyboard = {102: "А", 1073: "Б", 100: "В", 117: "Г", 108: "Д", 116: "Е", 1105: "Ё", 1078: "Ж", 112: "З", 98: "И", 113: "Й",
+keyboard = {102: "А", 44: "Б", 100: "В", 117: "Г", 108: "Д", 116: "Е", 96: "Ё", 59: "Ж", 112: "З", 98: "И", 113: "Й",
 114: "К", 107: "Л", 118: "М", 121: "Н", 106: "О", 103: "П", 104: "Р", 99: "С", 110: "Т", 101: "У", 97: "Ф", 1093: "Х", 
 119: "Ц", 120: "Ч", 105: "Ш", 111: "Щ", 1098: "Ъ", 115: "Ы", 109: "Ь", 1101: "Э", 1102: "Ю", 122: "Я"}
 
 words = ['кот', 'сок']
+n = rnd.choice(words)
+#
 
-words = words[rnd.randrange(2)]
 
 def line(color, x1, y1, x2, y2, thickness):
     pg.draw.line(screen, color, (x1, y1), (x2, y2), thickness)
+#def circle(color,)
 
 # -------init pygame--------------
 pg.init()
@@ -46,11 +50,20 @@ while running:
             running = False
         if event.type == pg.KEYDOWN:
             print(event.key)
-        #if 
+        l = keys[event.key]
+        spots = []
+        print (event.key, l)
+
+        for i, s in enumerate(n)
+        if l == s:
+            spots.append(i)
+            user_symbols.append(i)
+            if len(spots) > 0: choice = RIGHT
+            else: choice = WRONG
         
     line(BLACK, 200, 10, 200, 300, LINE_WIDTH)
     line(BLACK, 200, 10, 330, 10, LINE_WIDTH)
-    line(BLACK, 330, 10, 330, 100, LINE_WIDTH)
+    line(BLACK, 330, 10, 330, 60, LINE_WIDTH)
     
     pg.display.flip()
     
